394b917e040573144f7640ccc8ffb83665b7de86
crystaldevalt <liamlessard70@gmail.com>
2026-07-06 15:57:53 -0400

    update theme.js

--- theme.js
+++ theme.js
@@ -1,11 +1,12 @@
 export function initializeThemeToggle(buttonId) {
     const toggleButton = document.getElementById(buttonId);
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-mode');
+    if (!toggleButton) return;
+    if (localStorage.getItem('theme') === 'light') {
+        document.body.classList.add('light-mode');
     }
     toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        const isDarkMode = document.body.classList.contains('dark-mode');
-        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+        document.body.classList.toggle('light-mode');
+        const isLightMode = document.body.classList.contains('light-mode');
+        localStorage.setItem('theme', isLightMode ? 'light' : 'dark');
     });
 }
