a93736d0ec0b43b05f40d0545c4d5f5bc3a66a1f
crystaldevalt <liamlessard70@gmail.com>
2026-07-07 02:37:27 -0400

    update effects.js

--- effects.js
+++
@@ -1,9 +0,0 @@
-// effects.js
-alert("Script is loading!"); // This will pop up a box on your phone if it works
-
-const element = document.getElementById("typewriter-text");
-if (element) {
-    element.textContent = "Test worked!";
-} else {
-    alert("Element not found!");
-}
